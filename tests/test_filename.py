d993d57e3cdd2c6687463a187b48366f86c03db4
Gavin Tronset <gtronset@gmail.com>
2022-11-05 16:35:40 -0700

    Add character to test

--- tests/test_filename.py
+++ tests/test_filename.py
@@ -74,7 +74,7 @@ class CopyFileArtifactsFilename(CopyFileArtifactsTestCase):
 
         self._create_file(
             self.album_path,
-            b"CoolSName: Album&Tag.log",
+            b"Cool$Name: Album&Tag.log",
         )
         medium = self._create_medium(
             os.path.join(self.album_path, b"track_1.mp3"),
@@ -89,7 +89,7 @@ class CopyFileArtifactsFilename(CopyFileArtifactsTestCase):
             b"Tag Artist",
             b"Album_ Subtitle",
             beets.util.bytestring_path(
-                "Album_ Subtitle - CoolSName_ Album&Tag.log"
+                "Album_ Subtitle - Cool$Name_ Album&Tag.log"
             ),
         )
 
