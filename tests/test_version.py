44623e73c5c0f164cbcfad256130570dbb31b99d
Gavin Tronset <gtronset@gmail.com>
2023-05-14 14:44:06 -0700

    Add toml to test package version is correct

---
+++ tests/test_version.py
@@ -0,0 +1,32 @@
+"""
+Tests that the version specified for the plugin matches the value in pyproject.
+"""
+
+import toml  # type: ignore # pylint: disable=import-error
+
+import beetsplug
+from tests.helper import FiletoteTestCase
+
+
+class FiletoteVersionTest(FiletoteTestCase):
+    """
+    Tests that the version specified for the plugin matches the value in
+    pyproject.
+    """
+
+    def setUp(self, audible_plugin: bool = False) -> None:
+        """Provides shared setup for tests."""
+        super().setUp()
+
+    def test_version_matches(self) -> None:
+        """Ensure that the Filetote version is properly reflected in the right
+        areas."""
+
+        plugin_version = beetsplug.__version__
+
+        with open("./pyproject.toml", "r", encoding="utf-8") as pyproject_file:
+            data = toml.load(pyproject_file)
+
+            toml_version = data["tool"]["poetry"]["version"]
+
+            self.assertions.assertEqual(plugin_version, toml_version)
