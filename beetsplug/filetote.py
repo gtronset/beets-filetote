9db938f6052f275db48a25f33d0916b81686c9f9
Gavin Tronset <gtronset@gmail.com>
2023-11-14 20:01:06 -0800

    Refactor Tox settings

--- beetsplug/filetote.py
+++ beetsplug/filetote.py
@@ -21,7 +21,7 @@ from .filetote_dataclasses import (
 from .mapping_model import FiletoteMappingFormatted, FiletoteMappingModel
 
 if TYPE_CHECKING:
-    from beets import importer
+    from beets.importer import ImportSession
     from beets.library import Item, Library
 
 
@@ -89,7 +89,7 @@ class FiletotePlugin(BeetsPlugin):
 
         return path_formats
 
-    def _register_session_settings(self, session: "importer.ImportSession") -> None:
+    def _register_session_settings(self, session: "ImportSession") -> None:
         """
         Certain settings are only available and/or finalized once the
         Beets import session begins.
