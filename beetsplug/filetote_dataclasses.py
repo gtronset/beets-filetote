6496746030e6f35ea58182fa5419288384bdc361
Gavin Tronset <gtronset@gmail.com>
2023-11-18 14:40:27 -0800

    Refactor Config validation & add tests

--- beetsplug/filetote_dataclasses.py
+++ beetsplug/filetote_dataclasses.py
@@ -1,7 +1,7 @@
 """ Dataclasses for Filetote representing Settings/Config-related content along with
 data used in processing extra files/artifacts. """
 
-from dataclasses import Field, asdict, dataclass, field, fields
+from dataclasses import asdict, dataclass, field, fields
 from sys import version_info
 from typing import Any, Dict, List, Optional, Union
 
@@ -20,12 +20,6 @@ if version_info >= (3, 10):
     from typing import TypeAlias
 else:
     from typing_extensions import TypeAlias  # pylint: disable=import-error
-
-FieldType: TypeAlias = Any
-
-if version_info >= (3, 9):
-    FieldType = Field[Any]  # type: ignore[misc]
-
 
 StrSeq: TypeAlias = List[str]
 OptionalStrSeq: TypeAlias = Union[Literal[""], StrSeq]
@@ -73,9 +67,9 @@ class FiletotePairingData:
     extensions: Union[Literal[".*"], StrSeq] = DEFAULT_ALL_GLOB
 
     def __post_init__(self) -> None:
-        self.validate_types()
+        self._validate_types()
 
-    def validate_types(self) -> None:
+    def _validate_types(self) -> None:
         """Validate types for FileTote Pairing settings."""
         for field_ in fields(self):
             field_value = getattr(self, field_.name)
@@ -84,33 +78,15 @@ class FiletotePairingData:
             if field_.name in [
                 "enabled",
                 "pairing_only",
-            ] and not isinstance(field_value, field_type):
-                raise TypeError(
-                    f'Value for Filetote config key "{field_.name}" should be'
-                    f" `{field_type}`, got {type(field_value)}."
+            ]:
+                _validate_types_instance(
+                    ["pairing", field_.name], field_value, field_type
                 )
 
             if field_.name == "extensions":
-                self._validate_pairing_extensions(field_, field_value)
-
-    def _validate_pairing_extensions(self, field_: FieldType, field_value: Any) -> None:
-        if field_value != DEFAULT_ALL_GLOB and not isinstance(field_value, list):
-            raise TypeError(
-                f'Value for Filetote Pairing config key "{field_.name}" should be the'
-                ' glob ".*" (string) or sequence/list of strings (type `List[str]`),'
-                f" got {type(field_value)}."
-            )
-
-        if field_value != DEFAULT_ALL_GLOB and not all(
-            isinstance(elem, str) for elem in field_value
-        ):
-            for elem in field_value:
-                if not isinstance(elem, str):
-                    raise TypeError(
-                        f'Value for Filetote Pairing config key "{field_.name}" should'
-                        " be a sequence/list of strings (type `List[str]`), got"
-                        f" {type(elem)}."
-                    )
+                _validate_types_str_eq(
+                    ["pairing", field_.name], field_value, DEFAULT_ALL_GLOB
+                )
 
 
 @dataclass
@@ -129,7 +105,7 @@ class FiletoteConfig:
     print_ignored: bool = False
 
     def __post_init__(self) -> None:
-        self.validate_types()
+        self._validate_types()
 
     def asdict(self) -> dict:  # type: ignore[type-arg]
         """Returns a `dict` version of the dataclass."""
@@ -142,10 +118,10 @@ class FiletoteConfig:
         if attr == "pairing":
             value = FiletotePairingData(**value)
 
-        self.validate_types(attr, value)
+        self._validate_types(attr, value)
         setattr(self, attr, value)
 
-    def validate_types(
+    def _validate_types(
         self, target_field: Optional[str] = None, target_value: Any = None
     ) -> None:
         """Validate types for FileTote Config settings."""
@@ -159,74 +135,115 @@ class FiletoteConfig:
             if field_.name in [
                 "session",
                 "pairing",
-            ] and not isinstance(type(field_value), type(field_type)):
-                raise TypeError(
-                    f'Value for Filetote config key "{field_.name}" should be'
-                    f" `{field_type}`, got {type(field_value)}."
-                )
+            ]:
+                _validate_types_instance([field_.name], field_value, field_type)
 
             if field_.name in ["extensions", "filenames", "exclude"]:
-                self._validate_str_eq(field_, field_value)
+                _validate_types_str_eq([field_.name], field_value, DEFAULT_EMPTY)
 
             if field_.name == "patterns":
-                self._validate_patterns_dict(field_value)
+                _validate_types_dict(
+                    [field_.name], field_value, field_type=List, list_subtype=str
+                )
 
             if field_.name == "paths":
-                self._validate_paths_dict(field_value)
+                _validate_types_dict([field_.name], field_value, field_type=Template)
 
-            if field_.name == "print_ignored" and not isinstance(
-                field_value, field_type
-            ):
-                raise TypeError(
-                    f'Value for Filetote config key "{field_.name}" should be'
-                    f" `{field_type}`, got {type(field_value)}."
-                )
+            if field_.name == "print_ignored":
+                _validate_types_instance([field_.name], field_value, field_type)
 
-    def _validate_str_eq(self, field_: FieldType, field_value: Any) -> None:
-        if field_value != DEFAULT_EMPTY and not isinstance(field_value, list):
-            raise TypeError(
-                f'Value for Filetote config key "{field_.name}" should be a empty'
-                " string, an empty list, or sequence/list of strings (type"
-                f" `List[str]`), got {type(field_value)}."
+
+def _validate_types_instance(
+    field_name: List[str],
+    field_value: Any,
+    field_type: Any,
+) -> None:
+    """
+    A simple `instanceof` comparison. If present, `typewrape` will enable both
+    `field_value` and `field_type` to be wrapped by a `type()`.
+    """
+    if not isinstance(field_value, field_type):
+        _raise_type_validation_error(
+            field_name,
+            field_type,
+            field_value,
+        )
+
+
+def _validate_types_dict(
+    field_name: List[str],
+    field_value: Dict[Any, Any],
+    field_type: Any,
+    list_subtype: Optional[Any] = None,
+) -> None:
+    for key, value in field_value.items():
+        if not isinstance(key, str):
+            _raise_type_validation_error(
+                field_name,
+                "string (`str`)",
+                key,
+                key_name=key,
             )
 
-        if field_value != DEFAULT_EMPTY and not all(
-            isinstance(elem, str) for elem in field_value
-        ):
-            for elem in field_value:
-                if not isinstance(elem, str):
-                    raise TypeError(
-                        f'Value for Filetote config key "{field_.name}" should be a'
-                        " sequence/list of strings (type `List[str]`), got"
-                        f" {type(elem)}."
+        if not isinstance(value, field_type):
+            _raise_type_validation_error(field_name, "string (`str`)", value, key, True)
+
+        if list_subtype:
+            for elem in value:
+                if not isinstance(elem, list_subtype):
+                    _raise_type_validation_error(
+                        field_name,
+                        f"(inner element of the list) {list_subtype}",
+                        elem,
                     )
 
-    def _validate_patterns_dict(self, field_value: Dict[Any, Any]) -> None:
-        for key, value in field_value.items():
-            if not isinstance(key, str):
-                raise TypeError(
-                    f'Key "{key}" should be a string (type `str`), got'
-                    f" {type(field_value)}."
+
+def _validate_types_str_eq(
+    field_name: List[str],
+    field_value: Any,
+    optional_default: str,
+) -> None:
+    if field_value != optional_default:
+        if not isinstance(field_value, list):
+            _raise_type_validation_error(
+                field_name,
+                f"literal `{optional_default}`, an empty list, or sequence/list of"
+                " strings (type `List[str]`)",
+                field_value,
+            )
+
+        for elem in field_value:
+            if not isinstance(elem, str):
+                _raise_type_validation_error(
+                    field_name,
+                    "sequence/list of strings (type `List[str]`)",
+                    elem,
                 )
 
-            if not isinstance(value, List) and all(
-                isinstance(elem, str) for elem in value
-            ):
-                raise TypeError(
-                    'Value for Filetote config key "patterns" should be of'
-                    f" type `List[str]`, got {type(field_value)}."
-                )
 
-    def _validate_paths_dict(self, field_value: Dict[Any, Any]) -> None:
-        for key, value in field_value.items():
-            if not isinstance(key, str):
-                raise TypeError(
-                    f'Key "{key}" should be a string (type `str`), got'
-                    f" {type(field_value)}."
-                )
+def _raise_type_validation_error(
+    field_name: List[str],
+    expected_type: Any,
+    value: Any = None,
+    key_name: Optional[Any] = None,
+    check_keys_value: Optional[bool] = False,
+) -> None:
+    part_type: str = "Value"
+    received_type: Any = type(value)
 
-            if not isinstance(value, Template):
-                raise TypeError(
-                    'Value for Filetote config key "patterns" should be of'
-                    f" type `Template`, got {type(value)}."
-                )
+    if key_name:
+        part_type = f'Key "{key_name}"'
+        received_type = type(key_name)
+
+    if check_keys_value:
+        part_type = f"{part_type}'s Value"
+        received_type = type(value)
+
+    raise TypeError(
+        f'{part_type} for Filetote config key "{_format_config_hierarchy(field_name)}"'
+        f" should be of type {expected_type}, got `{received_type}`"
+    )
+
+
+def _format_config_hierarchy(parts: List[str]) -> str:
+    return "".join([f"[{part}]" for part in parts])
