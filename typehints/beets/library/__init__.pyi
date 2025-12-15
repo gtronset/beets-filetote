4fe6fa539810a66223601cb119181fdf3e3c0ede
Gavin Tronset <gtronset@gmail.com>
2025-12-14 18:51:03 -0800

    Fix typing stubs

--- typehints/beets/library/__init__.pyi
+++ typehints/beets/library/__init__.pyi
@@ -1,12 +1,8 @@
-from collections.abc import Callable
 from re import Pattern
-from typing import Any
 
-from .dbcore import Database
-from .dbcore.db import Model
-
-class DefaultTemplateFunctions:
-    def functions(self) -> dict[str, Callable[..., Any]]: ...
+from ..dbcore import Database
+from ..dbcore.db import Model
+from .models import DefaultTemplateFunctions
 
 class Library(Database):
     path: bytes
@@ -28,3 +24,7 @@ class Item(LibModel):
     path: bytes
 
     def __init__(self) -> None: ...
+
+__all__ = [
+    "DefaultTemplateFunctions",
+]
