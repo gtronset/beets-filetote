f43890cc429e5e82cabed8d3f4b404b0fc0aec05
Gavin Tronset <gtronset@gmail.com>
2026-03-06 12:53:19 -0800

    Vendor DummyIO as a temporary stopgap (#272)
    ## Description
    
    As reported in https://github.com/gtronset/beets-filetote/issues/270,
    [recent updates to beets](https://github.com/beetbox/beets/pull/5049/)
    changes the beet's `DummyIO` test helper, which Filetote currently
    relies upon. This PR vendors that class as a stopgap to ensure the test
    suite for Filetote runs without issue on all supported beets versions.
    
    `master` also introduces https://github.com/beetbox/beets/pull/6280
    which requires a small config addition to `FiletoteDuplicateActionTest`.
    
    ## To Do
    
    - [x] ~Documentation (update `README.md`)~
    - [x] Changelog (add an entry to `CHANGELOG.md`)
    - [x] Tests

--- tests/_common.py
+++ tests/_common.py
@@ -8,10 +8,13 @@ import tempfile
 import unittest
 
 from pathlib import Path
+from typing import TYPE_CHECKING
 
 from beets import config, logging, util
-from beets.test._common import DummyIO  # noqa: PLC2701
 from beets.util import FilesystemError
+
+if TYPE_CHECKING:
+    from typing import TextIO
 
 # Propagate to root logger so nosetest can capture it
 log = logging.getLogger("beets")
@@ -104,6 +107,95 @@ def check_reflink() -> bool:
 HAVE_SYMLINK = check_symlink()
 HAVE_HARDLINK = check_hardlink()
 HAVE_REFLINK = check_reflink()
+
+
+class InputError(Exception):
+    def __init__(self, output: str | None = None):
+        self.output = output
+
+    def __str__(self) -> str:
+        msg = "Attempt to read with no input provided."
+        if self.output is not None:
+            msg += f" Output: {self.output!r}"
+        return msg
+
+
+class DummyOut:
+    encoding = "utf-8"
+
+    def __init__(self) -> None:
+        self.buf: list[str] = []
+
+    def write(self, s: str | bytes) -> None:
+        if isinstance(s, bytes):
+            s = s.decode(self.encoding, "replace")
+        self.buf.append(s)
+
+    def get(self) -> str:
+        return "".join(self.buf)
+
+    def flush(self) -> None:
+        pass
+
+    def clear(self) -> None:
+        self.buf = []
+
+
+class DummyIn:
+    encoding = "utf-8"
+
+    def __init__(self, out: DummyOut | None = None):
+        self.buf: list[str] = []
+        self.reads: int = 0
+        self.out = out
+
+    def add(self, s: str) -> None:
+        self.buf.append(f"{s}\n")
+
+    def close(self) -> None:
+        pass
+
+    def readline(self) -> str:
+        if not self.buf:
+            if self.out:
+                raise InputError(self.out.get())
+            else:
+                raise InputError()
+        self.reads += 1
+        return self.buf.pop(0)
+
+
+class DummyIO:
+    """Mocks input and output streams for testing UI code."""
+
+    def __init__(self) -> None:
+        self.stdout: DummyOut = DummyOut()
+        self.stdin: DummyIn = DummyIn(self.stdout)
+        self._orig_stdin: TextIO | None = None
+        self._orig_stdout: TextIO | None = None
+
+    def addinput(self, s: str) -> None:
+        self.stdin.add(s)
+
+    def getoutput(self) -> str:
+        res = self.stdout.get()
+        self.stdout.clear()
+        return res
+
+    def readcount(self) -> int:
+        return self.stdin.reads
+
+    def install(self) -> None:
+        self._orig_stdin = sys.stdin
+        self._orig_stdout = sys.stdout
+        sys.stdin = self.stdin
+        sys.stdout = self.stdout
+
+    def restore(self) -> None:
+        if self._orig_stdin is not None:
+            sys.stdin = self._orig_stdin
+        if self._orig_stdout is not None:
+            sys.stdout = self._orig_stdout
 
 
 class AssertionsMixin:
