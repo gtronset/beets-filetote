aec617b57ca2b95a8a2dc16c22143c7d3142e3fd
Gavin Tronset <gtronset@gmail.com>
2026-03-10 17:38:33 -0700

    Update pruning tests

--- tests/test_pruning.py
+++ tests/test_pruning.py
@@ -1,271 +1,234 @@
 """Tests pruning for the beets-filetote plugin."""
 
-import logging
-
 from typing import TYPE_CHECKING
 
-from beets import config
-
-from tests.helper import FiletoteTestCase
+import pytest
 
 if TYPE_CHECKING:
     from pathlib import Path
 
-log = logging.getLogger("beets")
+    from tests.pytest_beets_plugin.plugin_fixture import BeetsPluginFixture
 
 
-class FiletotePruningyTest(FiletoteTestCase):
+class TestPruning:
     """Tests to check that Filetote correctly "prunes" directories when
     it moves artifact files.
     """
 
-    def setUp(self, _other_plugins: list[str] | None = None) -> None:
+    @pytest.fixture(autouse=True)
+    def _setup(self, beets_plugin_env: "BeetsPluginFixture") -> None:
         """Provides shared setup for tests."""
-        super().setUp()
+        self.env = beets_plugin_env
 
-        self._create_flat_import_dir()
-
-        self.album_path: Path = self.import_dir / "the_album"
-
-        self._setup_import_session(autotag=False, move=True)
+        env = self.env
+        env.create_flat_import_dir()
+        env.setup_import_session(autotag=False, move=True)
 
     def test_prune_import_directory_when_emptied(self) -> None:
         """Check that plugin does not interfere with normal
         pruning of emptied import directories.
         """
-        config["filetote"]["extensions"] = ".*"
+        env = self.env
 
-        self._run_cli_command("import")
+        env.config["filetote"]["extensions"] = ".*"
 
-        self.assert_import_dir_exists()
-        self.assert_not_in_import_dir("the_album")
+        env.run_cli_command("import")
+
+        env.assert_import_dir_exists()
+        env.assert_not_in_import_dir("the_album")
 
     def test_prune_import_subdirectory_only_not_above(self) -> None:
         """Check that plugin only prunes nested folder when specified."""
-        self._setup_import_session(
+        env = self.env
+        album_path = env.import_dir / "the_album"
+
+        env.setup_import_session(
             autotag=False,
-            import_dir=self.album_path,
+            import_dir=album_path,
             move=True,
         )
-        config["filetote"]["extensions"] = ".*"
-        self._run_cli_command("import")
+        env.config["filetote"]["extensions"] = ".*"
 
-        self.assert_import_dir_exists(self.import_dir)
-        self.assert_not_in_import_dir("the_album")
+        env.run_cli_command("import")
+
+        env.assert_import_dir_exists(env.import_dir)
+        env.assert_not_in_import_dir("the_album")
 
     def test_prunes_empty_artifact_subdirectory_on_move(self) -> None:
         """Tests that when an artifact is moved from a subdirectory within an album,
         the empty subdirectory is correctly pruned.
         """
-        artwork_dir_path: Path = self.album_path / "Artwork"
+        env = self.env
+        album_path = env.import_dir / "the_album"
+        artwork_dir_path: Path = album_path / "Artwork"
 
-        self.create_file(artwork_dir_path / "background.jpg")
+        env.create_file(artwork_dir_path / "background.jpg")
 
-        config["filetote"]["patterns"] = {"artwork": ["[aA]rtwork/"]}
-        config["filetote"]["extensions"] = ".*"
-        config["filetote"]["paths"] = {
-            "pattern:artwork": self.fmt_path("$albumpath", "art", "$old_filename")
+        env.config["filetote"]["patterns"] = {"artwork": ["[aA]rtwork/"]}
+        env.config["filetote"]["extensions"] = ".*"
+        env.config["filetote"]["paths"] = {
+            "pattern:artwork": env.fmt_path("$albumpath", "art", "$old_filename")
         }
-        config["import"]["move"] = True
+        env.config["import"]["move"] = True
 
-        self._run_cli_command("import")
+        env.run_cli_command("import")
 
-        self.assert_in_lib_dir("Tag Artist/Tag Album/art/background.jpg")
+        env.assert_in_lib_dir("Tag Artist/Tag Album/art/background.jpg")
 
-        self.assert_not_in_import_dir("the_album/Artwork/background.jpg")
-
-        self.assert_not_in_import_dir("the_album/Artwork")
-        self.assert_not_in_import_dir("the_album")
+        env.assert_not_in_import_dir("the_album/Artwork/background.jpg")
+        env.assert_not_in_import_dir("the_album/Artwork")
+        env.assert_not_in_import_dir("the_album")
 
     def test_prune_import_expands_user_import_path(self) -> None:
         """Check that plugin prunes and converts/expands the user parts of path if
         present.
         """
-        self._setup_import_session(
+        env = self.env
+        album_path = env.import_dir / "the_album"
+
+        env.setup_import_session(
             autotag=False,
-            import_dir=self.album_path,
+            import_dir=album_path,
             move=True,
         )
-        config["filetote"]["extensions"] = ".*"
-        self._run_cli_command("import")
+        env.config["filetote"]["extensions"] = ".*"
 
-        self.assert_import_dir_exists(self.import_dir)
-        self.assert_not_in_import_dir("the_album")
+        env.run_cli_command("import")
+
+        env.assert_import_dir_exists(env.import_dir)
+        env.assert_not_in_import_dir("the_album")
 
     def test_prune_reimport_move(self) -> None:
         """Check that plugin prunes to the root of the library when reimporting
         from library.
-
-        Setup subsequent import directory of the following structure:
-
-            testlib_dir/
-                Tag Artist/
-                    Tag Album/
-                        Tag Title 1.mp3
-                        Tag Title 2.mp3
-                        Tag Title 3.mp3
-                        artifact.file
-                        artifact2.file
         """
-        config["filetote"]["extensions"] = ".file"
+        env = self.env
 
-        log.debug("--- initial import")
-        self._run_cli_command("import")
+        env.config["filetote"]["extensions"] = ".file"
 
-        self.lib.path_formats[0] = (
+        env.log.debug("--- initial import")
+        env.run_cli_command("import")
+
+        env.lib.path_formats[0] = (
             "default",
-            self.fmt_path("1$artist", "$album", "$title"),
+            env.fmt_path("1$artist", "$album", "$title"),
         )
-        self._setup_import_session(autotag=False, import_dir=self.lib_dir, move=True)
+        env.setup_import_session(autotag=False, import_dir=env.lib_dir, move=True)
 
-        log.debug("--- second import")
+        env.log.debug("--- second import")
+        env.run_cli_command("import")
 
-        self._run_cli_command("import")
-
-        self.assert_not_in_lib_dir("Tag Artist/Tag Album")
-        self.assert_not_in_lib_dir("Tag Artist")
-        self.assert_in_lib_dir("1Tag Artist/Tag Album/artifact.file")
+        env.assert_not_in_lib_dir("Tag Artist/Tag Album")
+        env.assert_not_in_lib_dir("Tag Artist")
+        env.assert_in_lib_dir("1Tag Artist/Tag Album/artifact.file")
 
     def test_prune_reimport_copy(self) -> None:
         """Ensure directories are pruned when reimporting with 'copy'. The
         operation gets changed to `move` when the media file is already in the
         library (hence, reimport).
-
-        Setup subsequent import directory of the following structure:
-
-            testlib_dir/
-                Tag Artist/
-                    Tag Album/
-                        Tag Title 1.mp3
-                        Tag Title 2.mp3
-                        Tag Title 3.mp3
-                        artifact.file
-                        artifact2.file
         """
-        config["filetote"]["extensions"] = ".file"
+        env = self.env
 
-        log.debug("--- initial import")
-        self._run_cli_command("import")
+        env.config["filetote"]["extensions"] = ".file"
 
-        self.lib.path_formats[0] = (
+        env.log.debug("--- initial import")
+        env.run_cli_command("import")
+
+        env.lib.path_formats[0] = (
             "default",
-            self.fmt_path("1$artist", "$album", "$title"),
+            env.fmt_path("1$artist", "$album", "$title"),
         )
-        self._setup_import_session(autotag=False, import_dir=self.lib_dir, copy=True)
+        env.setup_import_session(autotag=False, import_dir=env.lib_dir, copy=True)
 
-        log.debug("--- second import")
+        env.log.debug("--- second import")
+        env.run_cli_command("import")
 
-        self._run_cli_command("import")
-
-        self.assert_not_in_lib_dir("Tag Artist/Tag Album")
-        self.assert_not_in_lib_dir("Tag Artist")
-        self.assert_in_lib_dir("1Tag Artist/Tag Album/artifact.file")
+        env.assert_not_in_lib_dir("Tag Artist/Tag Album")
+        env.assert_not_in_lib_dir("Tag Artist")
+        env.assert_in_lib_dir("1Tag Artist/Tag Album/artifact.file")
 
     def test_prune_reimport_query(self) -> None:
         """Check that plugin prunes to the root of the library when reimporting
         from library using `import` with a query.
-
-        Setup subsequent import directory of the following structure:
-
-            testlib_dir/
-                New Tag Artist/
-                    Tag Album/
-                        Tag Title 1.mp3
-                        Tag Title 2.mp3
-                        Tag Title 3.mp3
-                        artifact.file
-                        artifact2.file
         """
-        config["filetote"]["extensions"] = ".file"
+        env = self.env
 
-        log.debug("--- initial import")
-        self._run_cli_command("import")
+        env.config["filetote"]["extensions"] = ".file"
 
-        self.lib.path_formats = [
-            ("default", self.fmt_path("New Tag Artist", "$album", "$title")),
+        env.log.debug("--- initial import")
+        env.run_cli_command("import")
+
+        env.lib.path_formats = [
+            ("default", env.fmt_path("New Tag Artist", "$album", "$title")),
         ]
-        self._setup_import_session(query="artist", autotag=False, move=True)
+        env.setup_import_session(query="artist", autotag=False, move=True)
 
-        log.debug("--- second import")
-        self._run_cli_command("import")
+        env.log.debug("--- second import")
+        env.run_cli_command("import")
 
-        self.assert_not_in_lib_dir("Tag Artist/Tag Album")
-        self.assert_not_in_lib_dir("Tag Artist")
-        self.assert_in_lib_dir("New Tag Artist/Tag Album/artifact.file")
+        env.assert_not_in_lib_dir("Tag Artist/Tag Album")
+        env.assert_not_in_lib_dir("Tag Artist")
+        env.assert_in_lib_dir("New Tag Artist/Tag Album/artifact.file")
 
     def test_prune_move_query(self) -> None:
         """Check that plugin prunes any remaining empty album folders when using
-        the  `move` with a query.
-
-        Setup subsequent import directory of the following structure:
-
-            testlib_dir/
-                New Tag Artist/
-                    Tag Album/
-                        Tag Title 1.mp3
-                        Tag Title 2.mp3
-                        Tag Title 3.mp3
-                        artifact.file
-                        artifact2.file
+        the `move` with a query.
         """
-        config["filetote"]["extensions"] = ".file"
+        env = self.env
 
-        log.debug("--- initial import")
-        self._run_cli_command("import")
+        env.config["filetote"]["extensions"] = ".file"
 
-        self.lib.path_formats = [
-            ("default", self.fmt_path("New Tag Artist", "$album", "$title")),
+        env.log.debug("--- initial import")
+        env.run_cli_command("import")
+
+        env.lib.path_formats = [
+            ("default", env.fmt_path("New Tag Artist", "$album", "$title")),
         ]
 
-        log.debug("--- run mover")
-        self._run_cli_command("move", query="artist:'Tag Artist'")
+        env.log.debug("--- run mover")
+        env.run_cli_command("move", query="artist:'Tag Artist'")
 
-        self.assert_not_in_lib_dir("Tag Artist/Tag Album")
-        self.assert_not_in_lib_dir("Tag Artist")
-        self.assert_in_lib_dir("New Tag Artist/Tag Album/artifact.file")
+        env.assert_not_in_lib_dir("Tag Artist/Tag Album")
+        env.assert_not_in_lib_dir("Tag Artist")
+        env.assert_in_lib_dir("New Tag Artist/Tag Album/artifact.file")
 
     def test_prune_modify_query(self) -> None:
         """Check that plugin prunes any remaining empty album folders when using
-        the  `modify` with a query.
-
-        Setup subsequent import directory of the following structure:
-
-            testlib_dir/
-                New Tag Artist/
-                    Tag Album/
-                        Tag Title 1.mp3
-                        Tag Title 2.mp3
-                        Tag Title 3.mp3
-                        artifact.file
-                        artifact2.file
+        the `modify` with a query.
         """
-        config["filetote"]["extensions"] = ".file"
+        env = self.env
 
-        log.debug("--- initial import")
-        self._run_cli_command("import")
+        env.config["filetote"]["extensions"] = ".file"
 
-        self.lib.path_formats = [
-            ("default", self.fmt_path("$artist", "$album", "$title")),
+        env.log.debug("--- initial import")
+        env.run_cli_command("import")
+
+        env.lib.path_formats = [
+            ("default", env.fmt_path("$artist", "$album", "$title")),
         ]
 
-        log.debug("--- run modify")
-        self._run_cli_command(
-            "modify", query="artist:'Tag Artist'", mods={"artist": "New Tag Artist"}
+        env.log.debug("--- run modify")
+        env.run_cli_command(
+            "modify",
+            query="artist:'Tag Artist'",
+            mods={"artist": "New Tag Artist"},
         )
 
-        self.assert_not_in_lib_dir("Tag Artist/Tag Album")
-        self.assert_not_in_lib_dir("Tag Artist")
-        self.assert_in_lib_dir("New Tag Artist/Tag Album/artifact.file")
+        env.assert_not_in_lib_dir("Tag Artist/Tag Album")
+        env.assert_not_in_lib_dir("Tag Artist")
+        env.assert_in_lib_dir("New Tag Artist/Tag Album/artifact.file")
 
     def test_prunes_multidisc_nested(self) -> None:
         """Ensures that multidisc nested directories are pruned correctly on move."""
-        self._create_nested_import_dir()
-        self._setup_import_session(autotag=False, move=True)
+        env = self.env
 
-        config["filetote"]["extensions"] = ".*"
+        env.create_nested_import_dir()
+        env.setup_import_session(autotag=False, move=True)
 
-        self._run_cli_command("import")
+        env.config["filetote"]["extensions"] = ".*"
 
-        self.assert_not_in_import_dir("the_album/disc1")
-        self.assert_not_in_import_dir("the_album/disc2")
-        self.assert_not_in_import_dir("the_album")
+        env.run_cli_command("import")
+
+        env.assert_not_in_import_dir("the_album/disc1")
+        env.assert_not_in_import_dir("the_album/disc2")
+        env.assert_not_in_import_dir("the_album")
